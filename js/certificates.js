6286863341cb3f50cfc8e1a7a342d66323273713
Arturo <arturopsolis@gmail.com>
2024-04-08 12:40:50 -0600

    feat: dinamic certificates list

---
+++ js/certificates.js
@@ -0,0 +1,52 @@
+const certificates = [
+  {
+    name: "Developing Front-End Apps with React",
+    institution: "IBM",
+    link: "https://www.coursera.org/verify/X5878J9KQGZS",
+    code: "X5878J9KQGZS",
+  },
+  {
+    name: "Front End Development Libraries",
+    institution: "Freecodecamp",
+    link: "https://www.freecodecamp.org/certification/fcc9abb339b-24b4-464e-9960-2dac5d7054b8",
+    code: "fcc9abb339b-24b4-464e-9960-2dac5d7054b8",
+  },
+  {
+    name: "Javascript Algorithms and Data Structures",
+    institution: "Freecodecamp",
+    link: "https://www.freecodecamp.org/certification/fcc9abb339b-24b4-464e-9960-2dac5d7054b8/javascript-algorithms-and-data-structures",
+    code: "fcc9abb339b-24b4-464e-9960-2dac5d7054b8",
+  },
+  {
+    name: "Responsive Web Design",
+    institution: "Freecodecamp",
+    link: "https://www.freecodecamp.org/certification/fcc9abb339b-24b4-464e-9960-2dac5d7054b8/responsive-web-design",
+    code: "fcc9abb339b-24b4-464e-9960-2dac5d7054b8",
+  },
+  {
+    name: "Frontend Developer",
+    institution: "Platzi",
+    link: "https://platzi.com/@arturopacheco82/curso/1640-frontend-developer/diploma/detalle/",
+    code: "b54892c3-c7cf-422b-aec4-e903fc73b555",
+  },
+  {
+    name: "Foundations of UX Design",
+    institution: "Google",
+    link: "https://www.coursera.org/account/accomplishments/verify/Q5V95CF9J6GS",
+    code: "Q5V95CF9J6GS",
+  },
+  {
+    name: "Fundamentos de Marketing Digital",
+    institution: "Google Activate",
+    link: "https://learndigital.withgoogle.com/digitalgarage/validate-certificate-code",
+    code: "CEQ J43 DHK",
+  },
+  {
+    name: "Comercio Electrónico",
+    institution: "Google Activate",
+    link: "https://learndigital.withgoogle.com/digitalgarage/validate-certificate-code",
+    code: "WTD L4G 6Q7",
+  },
+];
+
+export default certificates;
--- js/main.js
+++ js/main.js
@@ -1,33 +1,52 @@
+import certificates from "./certificates.js";
+
+function createCertificateElement(certificate) {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `
+    <h3 class="certificate-title">${certificate.name}</h3>
+    <p>
+        <span class="certificate-institution">${certificate.institution}</span>
+        <span class="certificate-code">
+            <a href="${certificate.link}" target="_blank">
+                ${certificate.code}
+            </a>
+        </span>
+    </p>
+    `;
+  return listItem;
+}
+
+function displayCertificates() {
+  const certificatesList = document.getElementById("certificates");
+  certificatesList.innerHTML = "";
+  certificates.forEach((certificate) => {
+    const certificateElement = createCertificateElement(certificate);
+    certificatesList.appendChild(certificateElement);
+  });
+}
+
+// Llama a la función para mostrar los certificados al cargar la página
+window.onload = displayCertificates;
 
 let btnContacto = document.getElementById("btn-contacto");
 
-document.addEventListener('scroll',(event) => {
+document.addEventListener("scroll", (event) => {
+  // handle scroll event
+  console.log(this.scrollY);
 
-        // handle scroll event
-        console.log(this.scrollY);
-
-        if( this.scrollY < 176 ){
-            btnContacto.classList.add("hidden");
-            
-        }else if(this.scrollY > 176 && this.scrollY < 1398){
-            btnContacto.classList.remove("hidden");
-            btnContacto.style.backgroundPosition = "0px center";
-
-        }else if(this.scrollY > 1398 && this.scrollY < 1686){
-            btnContacto.classList.remove("hidden");
-            btnContacto.style.backgroundPosition = "-100px center";
-
-        }else if(this.scrollY > 1686 && this.scrollY < 2800){
-            btnContacto.classList.remove("hidden");
-            btnContacto.style.backgroundPosition = "-200px center";
-        }
-        else if(this.scrollY > 2800){
-            btnContacto.classList.remove("hidden");
-            btnContacto.style.backgroundPosition = "-200px center";
-        }
-
-
-
-        
-    }
-);
+  if (this.scrollY < 176) {
+    btnContacto.classList.add("hidden");
+  } else if (this.scrollY > 176 && this.scrollY < 1398) {
+    btnContacto.classList.remove("hidden");
+    btnContacto.style.backgroundPosition = "0px center";
+  } else if (this.scrollY > 1398 && this.scrollY < 1686) {
+    btnContacto.classList.remove("hidden");
+    btnContacto.style.backgroundPosition = "-100px center";
+  } else if (this.scrollY > 1686 && this.scrollY < 2800) {
+    btnContacto.classList.remove("hidden");
+    btnContacto.style.backgroundPosition = "-200px center";
+  } else if (this.scrollY > 2800) {
+    btnContacto.classList.remove("hidden");
+    btnContacto.style.backgroundPosition = "-200px center";
+  }
+});
