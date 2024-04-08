27f4a83df0664403c2d52b04bae836082f41196a
Arturo <arturopsolis@gmail.com>
2024-04-08 12:51:35 -0600

    fix: menu-redes, btn contacto fixed

--- js/main.js
+++ js/main.js
@@ -25,28 +25,27 @@ function displayCertificates() {
   });
 }
 
-// Llama a la función para mostrar los certificados al cargar la página
-window.onload = displayCertificates;
-
 let btnContacto = document.getElementById("btn-contacto");
 
 document.addEventListener("scroll", (event) => {
   // handle scroll event
-  console.log(this.scrollY);
+  console.log(window.scrollY);
 
-  if (this.scrollY < 176) {
+  if (window.scrollY < 176) {
     btnContacto.classList.add("hidden");
-  } else if (this.scrollY > 176 && this.scrollY < 1398) {
+  } else if (window.scrollY > 176 && window.scrollY < 1398) {
     btnContacto.classList.remove("hidden");
     btnContacto.style.backgroundPosition = "0px center";
-  } else if (this.scrollY > 1398 && this.scrollY < 1686) {
+  } else if (window.scrollY > 1398 && window.scrollY < 1686) {
     btnContacto.classList.remove("hidden");
     btnContacto.style.backgroundPosition = "-100px center";
-  } else if (this.scrollY > 1686 && this.scrollY < 2800) {
+  } else if (window.scrollY > 1686 && window.scrollY < 2800) {
     btnContacto.classList.remove("hidden");
     btnContacto.style.backgroundPosition = "-200px center";
-  } else if (this.scrollY > 2800) {
+  } else if (window.scrollY > 2800) {
     btnContacto.classList.remove("hidden");
     btnContacto.style.backgroundPosition = "-200px center";
   }
 });
+
+window.onload = displayCertificates;
