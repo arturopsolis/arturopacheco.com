62d275b87766384a3d5f111915cd415522e6f54b
Arturo <arturopsolis@gmail.com>
2024-10-17 23:05:48 -0600

    fix footer

---
+++ footer.php
@@ -0,0 +1,96 @@
+<!-- FOOTER -->
+<footer>
+
+	<div class="prefooterBar">
+		<div class="container">
+			<div class="row ml-none mr-none">
+    			<div class="col-6">
+					<h4>¿Por qué Punto Griego?</h4>
+ 					<h5>// Porque cambiamos la tendencia del mármol //</h5>
+    			</div>
+    			<div class="col-6 t-right">
+  					<h4>Te asesoramos con gusto</h4>
+   					<p><!--i class="fa fa-mobile" aria-hidden="true"></i> Ventas <a href="tel:5523284880">5523284880</a> |--> <i class="fa fa-truck" aria-hidden="true"></i> Entregas <a href="tel:5539398603">5539398603</a>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+    <!--  / container fullwidth -->
+    <div class="container">
+    
+        <!-- row footer -->
+        <div class="row">
+            
+            <!-- Links -->
+            <div class="col-md-4">
+                <ul class="list raleway500">
+                    <li>
+                        <a href="distribuidores.php">Distribuidores</a>
+                    </li>
+                    <li>
+                        <a href="proyectos-corporativos.php">Proyectos Corporativos</a>
+                    </li>
+                    <li>
+                        <a href="preguntas-frecuentes.php">Preguntas Frecuentes</a>
+                    </li>
+                    <li>
+                        <a href="aviso-de-privacidad.php">Aviso de privacidad</a>
+                    </li>
+                </ul>
+            </div>
+            
+            <!-- Redes Sociales -->
+            <div class="col-md-4">
+                <ul class="xd_redes-menu">
+                    <li>
+                        <a href="tel:+5539398603"><i class="fab fa-whatsapp"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/puntogriego/" target="_blank"><i class="fab fa-instagram"></i></a>
+                    </li>
+                    <li>
+                        <a href="" target="_blank"><i class="fab fa-facebook"></i></a>
+                    </li>
+                </ul>
+            </div>
+            
+            <!-- PUNTO GRIEGO SVG-->
+            <div class="col-md-4 onlyDesk">
+            	<img class="xd_30" src="images/laccon_loading.svg" />
+            </div>
+            
+        </div>
+        <!-- / row footer -->
+    </div>
+    <!--  / container fullwidth -->
+</footer>
+
+<!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v7.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="237163993473117"
+  theme_color="#13cf13"
+  logged_in_greeting="¡Hola! Gracias por ponerte en contacto con nosotros. ¿Cuáles son tus dudas? "
+  logged_out_greeting="¡Hola! Gracias por ponerte en contacto con nosotros. ¿Cuáles son tus dudas? ">
+      </div>
+       
+       
